b76e0e6967f6dc82c2bb9974a4b3c314f753377f
Musny Mubarak <119862882+musnymubarak@users.noreply.github.com>
2024-12-03 16:54:15 +0000

    On the left side, display the flowers, their image, name, and price.

--- ica01/src/components/Product.js
+++ ica01/src/components/Product.js
@@ -1,13 +1,17 @@
 import '../assets/CSS/layout.css';
-export default function Product(){
+import { flowers } from './FlowerDB';
+export default function Product({flower}){
     
     return(
         <div className="grid-item">
 
             <div class="card">
-                <img  />
+                <img  
+                   src={require(`../assets/image/${flower.img}`)}
+                    alt={flowers.name}
+                />
                 <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
+                    <h5 class="card-title">{flower.name} Price: {flower.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
                         <input type="number" id="quantity" name="quantity" />
--- ica01/src/components/Products.js
+++ ica01/src/components/Products.js
@@ -1,5 +1,7 @@
 
 import '../assets/CSS/layout.css';
+import {flowers} from './FlowerDB'
+import Product from './Product';
 
 
 export default function Products(){
@@ -13,7 +15,9 @@ export default function Products(){
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
                     {
-                        //product
+                        flowers.map((flower) => (
+                            <Product key={flower.id} flower={flower} />
+                        ))
                     }
                 </div>
 
